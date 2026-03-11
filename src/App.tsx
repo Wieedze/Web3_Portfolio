42323545a805c684b2fd2e55b9698d94f1472d45
Wieedze <maxime.moodz@gmail.com>
2026-03-11 15:54:35 +0100

    feat: init vite react-ts with tailwind v4, framer-motion and three.js

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen">
+      <h1 className="text-4xl text-white text-center pt-20">Portfolio</h1>
+    </div>
+  )
+}
+
+export default App
