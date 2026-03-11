c16c3ac0a880260f1a3a525f7798b69318241b38
Wieedze <maxime.moodz@gmail.com>
2026-03-11 15:59:41 +0100

    feat: add PixelBlast background component from react-bits

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import PixelBlast from './components/Background/PixelBlast'
+
 function App() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen relative">
+      <div className="fixed inset-0 -z-10">
+        <PixelBlast
+          pixelSize={1}
+          color="#ffffff"
+          pixelSizeJitter={0.1}
+          speed={1}
+          edgeFade={0.5}
+          enableRipples={false}
+          patternDensity={0.85}
+        />
+      </div>
+
       <h1 className="text-4xl text-white text-center pt-20">Portfolio</h1>
     </div>
   )
